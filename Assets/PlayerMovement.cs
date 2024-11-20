9eeaa977805f58dbd5525c79d5f314ee837f58e7
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-11-20 19:25:17 +0200

    First commit

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,81 @@
+using UnityEngine;
+using UnityEngine.UIElements;
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private Animator anim;
+    private float moveX;
+    private bool facingRight = true;
+
+    [Header("Movement")]
+    [SerializeField] private float speed;
+    [SerializeField] private float jumpForce;
+
+    [Header("Ground Check")]
+    [SerializeField] private float groundCheckRadius;
+    [SerializeField] private Transform groundCheck;
+    [SerializeField] private LayerMask whatIsGround;
+    private bool isGrounded;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        FlipController();
+        AnimationController();
+        CheckCollision();
+        Movement();
+    }
+
+    private void AnimationController()
+    {
+        anim.SetFloat("xVelocity", rb.velocity.x);
+        anim.SetFloat("yVelocity", rb.velocity.y);
+        anim.SetBool("isGrounded", isGrounded);
+    }
+
+    private void FlipController()
+    {
+        if(rb.velocity.x > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if(rb.velocity.x < 0 && facingRight)
+        {
+            Flip();
+        }
+    }
+
+    private void Flip()
+    {
+        facingRight = !facingRight;
+        transform.Rotate(0, 180, 0);
+    }
+
+    private void CheckCollision()
+    {
+        isGrounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround);
+    }
+
+    private void Movement()
+    {
+        moveX = Input.GetAxisRaw("Horizontal");
+
+        rb.velocity = new Vector2(moveX * speed, rb.velocity.y);
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            rb.velocity = new Vector2(rb.velocity.x, jumpForce);
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(groundCheck.position, groundCheckRadius);
+    }
+}
