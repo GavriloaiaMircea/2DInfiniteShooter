d5f78333f1721eb345ab03db5f846ad2f7c21fe8
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-11-26 19:50:13 +0200

    Reload and fixed shooting bug

--- Assets/GunController.cs
+++ Assets/GunController.cs
@@ -1,6 +1,3 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class GunController : MonoBehaviour
@@ -9,25 +6,37 @@ public class GunController : MonoBehaviour
     [SerializeField] private Transform gun;
     [SerializeField] private float gunDistance = 1.2f;
     private bool gunFacingRight = true;
-    // Update is called once per frame
 
     [Header("Bullet")]
     [SerializeField] private GameObject bulletPrefab;
     [SerializeField] private float bulletSpeed;
+    public int currentBullets;
+    public int maxBullets = 15;
+
+    private void Start()
+    {
+        ReloadGun();
+    }
 
     void Update()
     {
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        Vector3 direction = mousePos - transform.position;
+        mousePos.z = 0;
+        Vector3 direction = (mousePos - gun.position).normalized;
+
 
         gun.rotation = Quaternion.Euler(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg);
-
         float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
         gun.position = transform.position + Quaternion.Euler(0, 0, angle) * new Vector3(gunDistance, 0, 0);
 
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot(direction);
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            ReloadGun();
         }
 
         GunFlipController(mousePos);
@@ -35,29 +44,43 @@ public class GunController : MonoBehaviour
 
     private void GunFlipController(Vector3 mousePos)
     {
-        if(mousePos.x<gun.position.x && gunFacingRight)
+        if (mousePos.x < gun.position.x && gunFacingRight)
         {
             FlipGun();
         }
-        else if(mousePos.x>gun.position.x && !gunFacingRight)
+        else if (mousePos.x > gun.position.x && !gunFacingRight)
         {
             FlipGun();
-        }   
+        }
     }
 
     private void FlipGun()
     {
         gunFacingRight = !gunFacingRight;
-        gun.localScale = new Vector3(gun.localScale.x, gun.localScale.y * -1, gun.localScale.z);   
+        gun.localScale = new Vector3(gun.localScale.x, gun.localScale.y * -1, gun.localScale.z);
     }
 
     public void Shoot(Vector3 direction)
     {
-        gunAnim.SetTrigger("Shoot");
+        if (currentBullets > 0)
+        {
+            currentBullets--;
+            gunAnim.SetTrigger("Shoot");
 
-        GameObject newBullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
-        newBullet.GetComponent<Rigidbody2D>().velocity = direction.normalized * bulletSpeed;
+            GameObject newBullet = Instantiate(bulletPrefab, gun.position, gun.rotation);
+            newBullet.GetComponent<Rigidbody2D>().velocity = direction * bulletSpeed;
 
-        Destroy(newBullet, 7);
+            Destroy(newBullet, 7);
+        }
+        else
+        {
+            Debug.Log("No bullets left!");
+        }
     }
+
+    private void ReloadGun()
+    {
+        currentBullets = maxBullets;
+    }
+
 }
